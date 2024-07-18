ea43c9e18ff3a5e86a7ccea17b0a7d20b0c8d4cf
Aditya Prasad <aditya.prasad@ibm.com>
2024-07-18 15:40:35 -0400

    mt bench chainer script

---
+++ run_mt_bench.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+model_path=$1
+model_id=$2
+bench_name="mt_bench"
+max_new_tokens=$4
+num_choices=$5
+num_gpus_per_model=$6
+num_gpus_total=$7
+max_gpu_memory=$8
+dtype=$9
+answer_dir_root=$10
+judge_model=$11
+baseline_model=$12
+mode=$13
+
+cd fastchat/llm_judge
+
+python gen_model_answer.py \
+    --model-path $model_path \
+    --model-id $model_id \
+    --bench-name $bench_name \
+    --num-choices $num_choices \
+    --num-gpus-per-model $num_gpus_per_model \
+    --num-gpus-total $num_gpus_total \
+    --max-gpu-memory $max_gpu_memory \
+    --dtype $dtype \
+    --answer-dir-root $answer_dir_root
+python gen_judgment.py \
+    --bench-name $bench_name \
+    --model-list $model_id \
+    --judge-model $judge_model \
+    --baseline-model $baseline_model \
+    --mode $mode \
+    --parallel 16 \
+    --answer-dir-root $answer_dir_root
+
+python show_result.py \
+    --bench-name $bench_name \
+    --model-list $model_id \
+    --judge-model $judge_model \
+    --baseline-model $baseline_model \
+    --mode $mode \
+    --answer-dir-root $answer_dir_root
